ee4cde664d9d54b3a5a3b8546c69c071a3b1c2d5
ryanplusplus <ryanplusplus@gmail.com>
2014-06-01 19:42:36 -0400

    hmm... rotation.

--- main.lua
+++ main.lua
@@ -1,14 +1,22 @@
+local angle = 0
+local width = 10
+local height = 10
+
 function love.load()
   love.graphics.setColor(0, 0, 0, 255)
   love.graphics.setBackgroundColor(255, 153, 0)
 end
 
-function love.update()
-
+function love.update(dt)
+  if love.keyboard.isDown('d') then
+    angle = angle + math.pi * dt
+  elseif love.keyboard.isDown('a') then
+    angle = angle - math.pi * dt
+  end
 end
 
 function love.draw()
-  love.graphics.circle("fill", 200, 300, 50, 50)
-  love.graphics.rectangle('fill', 300, 300, 100, 100)
-  love.graphics.arc('fill', 450, 300, 100, math.pi/5, math.pi/2)
+  love.graphics.rotate(angle)
+  love.graphics.setColor(0, 0, 225)
+  love.graphics.rectangle('fill', 300, 400, width, height)
 end

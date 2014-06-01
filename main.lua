c895f044bfc38967daa9e997ea915af376809452
ryanplusplus <ryanplusplus@gmail.com>
2014-06-01 18:34:43 -0400

    slightly fany fancier...

--- main.lua
+++ main.lua
@@ -1,3 +1,14 @@
+function love.load()
+  local myfont = love.graphics.newFont(45)
+  love.graphics.setFont(myfont)
+  love.graphics.setColor(0, 0, 0, 255)
+  love.graphics.setBackgroundColor(255, 153, 0)
+end
+
+function love.update()
+
+end
+
 function love.draw()
-  love.graphics.print('hello world!', 400, 300)
+  love.graphics.print('hello world!', 200, 200)
 end

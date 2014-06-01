4049030a865847f035b01bf6addff4f099105e1d
ryanplusplus <ryanplusplus@gmail.com>
2014-06-01 19:00:52 -0400

    simple shapes

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
 function love.load()
-  local myfont = love.graphics.newFont(45)
-  love.graphics.setFont(myfont)
   love.graphics.setColor(0, 0, 0, 255)
   love.graphics.setBackgroundColor(255, 153, 0)
 end
@@ -10,5 +8,7 @@ function love.update()
 end
 
 function love.draw()
-  love.graphics.print('hello world!', 200, 200)
+  love.graphics.circle("fill", 200, 300, 50, 50)
+  love.graphics.rectangle('fill', 300, 300, 100, 100)
+  love.graphics.arc('fill', 450, 300, 100, math.pi/5, math.pi/2)
 end

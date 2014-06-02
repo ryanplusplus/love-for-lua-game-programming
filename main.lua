3b8ad1c9c8a703637b98e32bda9bd526c44252c6
ryanplusplus <ryanplusplus@gmail.com>
2014-06-01 20:02:56 -0400

    moving a blue dot left, right, up, down

--- main.lua
+++ main.lua
@@ -1,22 +1,32 @@
-local angle = 0
 local width = 10
 local height = 10
 
 function love.load()
-  love.graphics.setColor(0, 0, 0, 255)
+  character = {}
+  character.x = 300
+  character.y = 400
+
   love.graphics.setBackgroundColor(255, 153, 0)
+
+  love.graphics.setColor(0, 0, 225)
 end
 
 function love.update(dt)
   if love.keyboard.isDown('d') then
-    angle = angle + math.pi * dt
-  elseif love.keyboard.isDown('a') then
-    angle = angle - math.pi * dt
+    character.x = character.x + 10 * dt
+  end
+  if love.keyboard.isDown('a') then
+    character.x = character.x - 10 * dt
+  end
+
+  if love.keyboard.isDown('w') then
+    character.y = character.y - 10 * dt
+  end
+  if love.keyboard.isDown('s') then
+    character.y = character.y + 10 * dt
   end
 end
 
 function love.draw()
-  love.graphics.rotate(angle)
-  love.graphics.setColor(0, 0, 225)
-  love.graphics.rectangle('fill', 300, 400, width, height)
+  love.graphics.rectangle('fill', character.x, character.y, width, height)
 end

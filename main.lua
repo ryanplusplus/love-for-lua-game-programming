06d590c1e4057df033e001386b4b593b5669d4a0
ryanplusplus <ryanplusplus@gmail.com>
2014-06-18 20:49:19 -0400

    Added background

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
 local loader = require('advanced-tile-loader/Loader')
 
+bg = love.graphics.newImage('res/background.png')
+
 function love.load()
   love.graphics.setBackgroundColor(255, 153, 0)
   map = loader.load('res/map.tmx')
 end
 
 function love.draw()
+  love.graphics.draw(bg)
   map:draw()
 end

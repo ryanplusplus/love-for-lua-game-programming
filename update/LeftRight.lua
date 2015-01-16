a4d5b00170f8d95188a6482868ff973c11af0749
ryanplusplus <ryanplusplus@gmail.com>
2015-01-15 20:57:20 -0500

    Fix for screwy direction at idle

--- update/LeftRight.lua
+++ update/LeftRight.lua
@@ -3,10 +3,10 @@ return function(key_held)
     for entity in pairs(scene:entities_with('animation', 'position', 'left_right', 'jump', 'direction')) do
       if key_held[entity.left_right.left_key] then
         entity.velocity.x = -entity.left_right.speed
-        entity.direction = 'left'
+        entity.direction = -1
       elseif key_held[entity.left_right.right_key] then
         entity.velocity.x = entity.left_right.speed
-        entity.direction = 'right'
+        entity.direction = 1
       else
         entity.velocity.x = 0
       end

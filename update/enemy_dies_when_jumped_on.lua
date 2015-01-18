9eb4f65824669b37efe56d546a1b5a112531ff51
ryanplusplus <ryanplusplus@gmail.com>
2015-01-18 12:16:13 -0500

    Send death event when enemy dies

--- update/enemy_dies_when_jumped_on.lua
+++ update/enemy_dies_when_jumped_on.lua
@@ -1,7 +1,16 @@
 return function(scene, dt)
   for event in pairs(scene:entities_with('event', 'jumped_on')) do
     if event.jumped_on.jumpee.enemy then
-      event.jumped_on.jumpee.dead = true
+      local enemy = event.jumped_on.jumpee
+
+      enemy.dead = true
+
+      scene:new_entity({
+        event = true,
+        death = {
+          entity = enemy
+        }
+      })
     end
   end
 end

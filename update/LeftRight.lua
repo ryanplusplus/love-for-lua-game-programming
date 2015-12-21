e0cc6fdc5d2ba1e310ce5c0ebe197ae84981ef8d
Ryan Hartlage <ryanplusplus@gmail.com>
2015-12-20 21:22:25 -0500

    Corrected component list for left-right

--- update/LeftRight.lua
+++ update/LeftRight.lua
@@ -1,6 +1,6 @@
 return function(key_held)
   return function(scene, dt)
-    for entity in pairs(scene:entities_with('animation', 'position', 'left_right', 'jump', 'direction')) do
+    for entity in pairs(scene:entities_with('animation', 'velocity', 'left_right', 'jump', 'direction')) do
       if key_held[entity.left_right.left_key] then
         entity.velocity.x = -entity.left_right.speed
         entity.direction = -1
